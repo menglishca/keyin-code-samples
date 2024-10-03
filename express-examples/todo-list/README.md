4f07a2bd80735f2ab6291665a5e5d7a891c4983b
Matthew English <matt.english4@gmail.com>
2024-10-02 22:31:21 -0230

    Add todo list example
    - Add finished code for the todo list example
    - Like the simple greeting, code was originally built from the base repo provided to the class

---
+++ express-examples/todo-list/README.md
@@ -0,0 +1,5 @@
+# Todo List
+
+- A simple express app that allows a user to maintain a todo list
+    - List is stored in server memory and not persisted to a database
+- TODO: Add ability to delete items
---
+++ express-examples/todo-list/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const port = 3000;
+const app = express();
+const currentTasks = ["Buy Groceries"];
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+    response.render('index', { currentTasks: currentTasks});
+});
+
+app.post('/todos', (request, response) => {
+    const newTodo = request.body.todo;
+    if (newTodo) {
+        currentTasks.push(newTodo);
+    }
+    response.redirect('/');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
