910094789cf39e72245aaf80c2b838b9854d5e9b
Matthew English <matt.english4@gmail.com>
2024-11-14 09:00:22 -0330

    Add simple registration example

--- user-authentication-examples/simple-registration/complete/index.js
+++ user-authentication-examples/simple-registration/complete/index.js
@@ -16,34 +16,45 @@ app.use(session({
   saveUninitialized: false,
 }));
 
-// Fake user data for demonstration purposes
-const users = [
-    { username: 'testuser', password: bcrypt.hashSync('password123', saltRounds) }
-];
+
+const users = [];
+
+app.get('/register', (request, response) => {
+    const errorMessage = request.query.error || null;
+    return response.render('register', { errorMessage })
+});
+
+app.post('/register', (request, response) => {
+    const { username, password } = request.body;
+    if (users.find((user) => user.username === username)) {
+      return response.status(400).render('register', {errorMessage: 'Username is taken'});
+    }
+
+    users.push({ username, password: bcrypt.hashSync(password, saltRounds) });
+    return response.redirect('/');
+});
+
+app.get('/login', (request, response) => {
+    const errorMessage = request.query.error || null;
+    return response.render('login', { errorMessage })
+});
 
 app.post('/login', (request, response) => {
     const { username, password } = request.body;
     const user = users.find((user) => user.username === username);
+
     if (!!user && bcrypt.compareSync(password, user.password)) {
         request.session.username = username;
         return response.redirect('/');
     }
 
-    return response.redirect('/?error=Invalid username or password');
+    return response.redirect('/login?error=Invalid username or password');
 });
 
 app.get('/', (request, response) => {
     const username = request.session.username;
     const errorMessage = request.query.error || null;
     response.render('index', { username, errorMessage });
-});
-
-app.get('/dashboard', (request, response) => {
-    if (!request.session.username) {
-        return response.redirect('/?error=You must be logged in to do that.');
-    }
-
-    return response.render('dashboard', {username: request.session.username});
 });
 
 app.post('/logout', (request, response) => {
